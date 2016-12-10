198d6bba1b0618f526fec4110447b21a8bff1aae
Beefstyles <glenwillis@gmail.com>
2016-12-10 17:19:42 +0800

    Scripts and mat.

---
+++ LudumDare37/Assets/Scripts/FPSPlayerButtonPress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSPlayerButtonPress : MonoBehaviour {
+
+    private RaycastHit hit;
+    private Camera fpsCamera;
+    StandardButton sb;
+
+
+    void Start()
+    {
+        fpsCamera = GetComponentInChildren<Camera>();
+    }
+	void Update ()
+    {
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, 100F))
+        {
+            if (Input.GetButtonDown("Fire1") && hit.transform.gameObject.tag=="Clickable")
+            {
+                sb = hit.transform.gameObject.GetComponent<StandardButton>();
+                sb.ButtonPressed = true;
+            }
+        }
+        
+		
+	}
+}
---
+++ LudumDare37/Assets/Scripts/StandardButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardButton : MonoBehaviour {
+
+    public bool ButtonPressed { get; set; }
+    public Material ButtonOnMat, ButtonOffMat;
+    private bool buttonOn;
+    private MeshRenderer gameObjMesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObjMesh = GetComponent<MeshRenderer>();
+        gameObjMesh.material = ButtonOffMat;
+        buttonOn = false;
+        ButtonPressed = false;
+		
+	}
+
+	void Update ()
+    {
+        if (ButtonPressed)
+        {
+            ButtonPressed = false;
+            if (buttonOn)
+            {
+                gameObjMesh.material = ButtonOffMat;
+                buttonOn = false;
+            }
+
+            else
+            {
+                gameObjMesh.material = ButtonOnMat;
+                buttonOn = true;
+            }
+        }
+	}
+}
