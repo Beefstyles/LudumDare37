097c03b3d5f50a403e3d9fe629ed39db2c213743
Beefstyles <glenwillis@gmail.com>
2016-12-10 23:11:37 +0800

    Added enums for actions.

---
+++ LudumDare37/Assets/Scripts/ButtonCharacteristics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PossibleActions
+{
+    One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Verb, Noun, Clear, Execute
+};
+
+public class ButtonCharacteristics : MonoBehaviour {
+
+    public PossibleActions pa;
+}
