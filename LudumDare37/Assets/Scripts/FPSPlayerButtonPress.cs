c9b97b2166feba3a692e17e841be5ad4a8654bb3
Beefstyles <glenwillis@gmail.com>
2016-12-10 21:39:52 +0800

    Now have a simple crosshair changing system.

--- LudumDare37/Assets/Scripts/FPSPlayerButtonPress.cs
+++ LudumDare37/Assets/Scripts/FPSPlayerButtonPress.cs
@@ -1,29 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSPlayerButtonPress : MonoBehaviour {
 
     private RaycastHit hit;
     private Camera fpsCamera;
     StandardButton sb;
-
+    public Image Crosshair;
+    public Sprite ActiveCrosshair, NormalCrosshair;
+    private bool ActiveCrosshairIsSet = false;
 
     void Start()
     {
         fpsCamera = GetComponentInChildren<Camera>();
+        Crosshair.sprite = NormalCrosshair;
     }
 	void Update ()
     {
+        if (ActiveCrosshairIsSet)
+        {
+            Crosshair.sprite = NormalCrosshair;
+            ActiveCrosshairIsSet = false;
+        }
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, 100F))
         {
-            if (Input.GetButtonDown("Fire1") && hit.transform.gameObject.tag=="Clickable")
+            if (hit.transform.gameObject.tag == "Clickable")
             {
-                sb = hit.transform.gameObject.GetComponent<StandardButton>();
-                sb.ButtonPressed = true;
+                if (!ActiveCrosshairIsSet)
+                {
+                    Crosshair.sprite = ActiveCrosshair;
+                    ActiveCrosshairIsSet = true;
+                }
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    sb = hit.transform.gameObject.GetComponent<StandardButton>();
+                    sb.ButtonPressed = true;
+                }
             }
         }
-        
+
 		
 	}
 }
