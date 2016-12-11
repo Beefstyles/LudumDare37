41b7157bc7242529dea975b33900068f93de6d25
Beefstyles <glenwillis@gmail.com>
2016-12-11 21:43:01 +0800

    Added a restart button.

---
+++ LudumDare37/Assets/Scripts/MasterControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterControllerScript : MonoBehaviour {
+
+    public bool PlayerMovementActive { get; set; }
+	
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
