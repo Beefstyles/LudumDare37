2f73568e39c43cfbeefc197423b54fa5c30aaf79
Beefstyles <glenwillis@gmail.com>
2016-12-11 21:53:12 +0800

    Added some kill conditions.

--- LudumDare37/Assets/Scripts/MasterControllerScript.cs
+++ LudumDare37/Assets/Scripts/MasterControllerScript.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MasterControllerScript : MonoBehaviour {
 
     public bool PlayerMovementActive { get; set; }
+    public Camera EndGameCamera;
+    DSKY_Control_Computer dskyControl;
+    public Text KillString;
 	
 	void Start ()
     {
-		
-	}
+        dskyControl = FindObjectOfType<DSKY_Control_Computer>();
+        EndGameCamera.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
 
 	}
+
+    public void EndGame(string killName)
+    {
+        EndGameCamera.enabled = true;
+        KillString.text = killName;
+    }
 }
